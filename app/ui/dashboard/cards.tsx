2683c9039166e8a2337f0cceca2e471cafa1671b
66070338-stack <66070338@kmitl.ac.th>
2025-10-04 06:20:51 +0700

    modified:   app/ui/dashboard/cards.tsx

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -15,7 +15,7 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
-    const {
+  const {
     numberOfInvoices,
     numberOfCustomers,
     totalPaidInvoices,
@@ -23,16 +23,14 @@ export default async function CardWrapper() {
   } = await fetchCardData();
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
